46f78e2a13ef2ace3071c446618f1420b907d551
JSapp.me <contact@jsapp.me>
2020-05-23 11:08:26 -0600

    email

--- functions/contact/contact.js
+++ functions/contact/contact.js
@@ -33,7 +33,34 @@ exports.handler = async function(event, context, callback) {
   }
   */
   ///
-  const payload = { 'engagement': { 'active': true, 'ownerId': 1, 'type': 'NOTE', 'timestamp': Date.now() }, 'associations': { 'contactIds': [901], 'companyIds': [ ], 'dealIds': [ ], 'ownerIds': [ ] }, 'metadata': { 'body': 'note body' } }
+  const payload = { 
+    'engagement': 
+      { 
+        'active': true, 
+        'ownerId': 1, 
+        'type': 'EMAIL', 
+        'timestamp': Date.now() 
+      }, 
+    'associations': 
+      { 
+        'contactIds': [901], 
+        'companyIds': [ ], 
+        'dealIds': [ ], 
+        'ownerIds': [ ] 
+      }, 
+    'metadata': 
+      {
+        "from": {
+          "email": senderEmail,
+          "firstName": senderFirstName,
+          "lastName": senderLastName,
+        },
+        "to": [{ "email": "JSapp.me <herve76@gmail.com>" }],
+        "subject": "This is the subject of the email",
+        "text": "This is the body of the email\n\n-Me"
+      }
+    }
+  }
   await hubspot.engagements.create(payload).then(data => { 
     console.log(data)
   })
